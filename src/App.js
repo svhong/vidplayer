3d0b09327a099bbc27931a4af91b9a6713350b0d
Sean <writeseanhong@gmail.com>
2020-03-26 08:09:16 -0500

    resetting application

--- src/App.js
+++ src/App.js
@@ -1,54 +1,7 @@
 import React, { Component } from "react";
-import SearchBar from "./components/SearchBar";
-import VideoList from "./components/VideoList"
-import VideoDetail from "./components/VideoDetail";
 
-import youtube from "./apis/youtube";
-
-import "./components/VideoItem.css";
-
-
-class App extends Component {
-
-    state = {
-        videos: [],
-        selectedVideo: null
-    };
-
-    onTermSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        this.setState({ videos: response.data.items })
-    }
-
-    onVideoSelect = (video) => {
-        console.log(video.snippet)
-        this.setState({ selectedVideo: video });
-    }
-
-
-    render() {
-        return (
-            <div className="ui container" style={{ marginTop: "10px" }}>
-                <SearchBar onTermSubmit={this.onTermSubmit} />
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList
-                                onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+const App = () => {
+    return <h1>Hey this will be a reboot</h1>
 }
 
 export default App;
